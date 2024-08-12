a55787650c32c6dd220575304dfd78eeb016017e
Yuxuan Xie <yx353@cam.ac.uk>
2024-08-12 20:12:22 +0100

    finished animation functions for greedy 1.0

--- src/graph.py
+++ src/graph.py
@@ -99,23 +99,56 @@ def greedy(graph, color_set_size, iterations):
         iterations_taken = i + 1
 
         # Plot graph to show recoloring 
-        # draw_graph(graph, graph_1_name, iterations_taken, pos)
+        draw_graph(graph, pos, graph_1_name, iterations_taken)
 
     return graph, cur_cost, itertions_taken
+
+
+def animate_greedy(graph, color_set_size, iterations):
+    cur_cost = calc_cost(graph)
+    color_set = list(range(color_set_size))
+    iterations_taken = 0
+
+    yield graph, cur_cost, iterations_taken # yield initial state
+
+    for i in range(iterations):
+        vertex_choice = None
+        color_choice = None
+        max_cost_reduction = 0
+
+        for vertex in graph.nodes:
+            ori_color = graph.nodes[vertex]['color']
+
+            for color in color_set:
+                delta_cost = calc_delta_cost(graph, vertex, ori_color, color)
+
+                if delta_cost > max_cost_reduction:
+                    max_cost_reduction = delta_cost
+                    vertex_choice = vertex
+                    color_choice = color
+
+        if max_cost_reduction == 0:
+            break
+        
+        # Recoloring 
+        graph.nodes[vertex_choice]['color'] = color_choice
+        cur_cost -= max_cost_reduction
+        iterations_taken = i + 1
+
+        yield graph, cur_cost, iterations_taken # yield updated state
+
 
 def reluctant(graph, color_set_size, iterations):
     pass
 
-def draw_graph(graph, graph_name, iteration_count, pos):
+def draw_graph(graph, pos, graph_name, iteration_count):
     '''
-    Draw the graph using Graphviz and save to an image file.
+    Draw the graph using Graphviz on the given axis
     '''
+
     vertex_colors = [color_map.get(str(graph.nodes[node].get('color', 0)), 'gray') for node in graph.nodes]
 
     edge_weights = nx.get_edge_attributes(graph, 'weight')
-
-    # Draw the graph
-    plt.figure(figsize=(6, 6))
 
     nx.draw_networkx(graph, pos, with_labels=True, node_color=vertex_colors, node_size=500, edge_color='black', font_color='white', font_size=10)
     nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_weights, rotate=False)
@@ -133,16 +166,50 @@ def draw_graph(graph, graph_name, iteration_count, pos):
     plt.savefig(graph_name)
     plt.show()
 
+
 # Animtation functions
+
+def animate(graph, color_set_size, iterations, pos, graph_name):
+    fig, ax = plt.subplots(figsize=(6, 6))
+
+    def update(frame_data):
+        graph, cur_cost, iteration_count = frame_data
+        ax.clear()
+
+        vertex_colors = [color_map.get(str(graph.nodes[node].get('color', 0)), 'gray') for node in graph.nodes]
+        edge_weights = nx.get_edge_attributes(graph, 'weight')
+
+        nx.draw_networkx(graph, pos, with_labels=True, node_color=vertex_colors, node_size=500, edge_color='black', font_color='white', font_size=10, ax=ax)
+        nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_weights, rotate=False, ax=ax)
+
+        ax.text(
+            0.95, 0.1, f'Iterations: {iteration_count}\nCost: {calc_cost(graph)}\nColors used: {len(set(nx.get_node_attributes(graph, "color").values()))}', 
+            horizontalalignment='right',
+            verticalalignment='center', 
+            transform=ax.transAxes,
+            fontsize=9, 
+            bbox=dict(facecolor='white', alpha=0.5, edgecolor='none')
+        )
+
+        ax.set_title(graph_name)
+
+    # Create an animation using the greedy function as a generator
+    ani = animation.FuncAnimation(
+        fig, update, frames=animate_greedy(graph, color_set_size, iterations), interval=1000, repeat=False
+    )
+    plt.show()
+
 
 if __name__ == '__main__':
     # Graph and color map paths
     graph_1_json_path = 'C:/Users/Yuxuan Xie/Desktop/Heuristics for combinatorial optimisation/Heuristics-for-combinatorial-optimisation/data/sample_graphs/graph1.json'
     color_map_path = 'C:/Users/Yuxuan Xie/Desktop/Heuristics for combinatorial optimisation/Heuristics-for-combinatorial-optimisation/data/color_map.json'
 
+    # Load graph and color map
     graph_1, graph_1_name = load_json_graph(graph_1_json_path)
     color_map = load_color_map(color_map_path)
 
+    # Initialise parameters
     # Define layout for graph visualiation, set vertex positions
     pos = nx.spring_layout(graph_1, seed=4) # force-directed algo
     # pos = nx.circular_layout(graph) # circle
@@ -152,7 +219,12 @@ if __name__ == '__main__':
     # pos = nx.spectral_layout(graph) # use eigenvectors of graph Laplacian matrix
     # pos = nx.draw_planar(graph) # planar graph
 
+    max_iterations = 10
+    color_set_size = 3
+
     # Apply optimising algorithm
-    # graph_1_greedy, cost, iterations_taken = greedy(graph_1, 3, 6)
+    graph_1_greedy, cost, iterations_taken = greedy(graph_1, color_set_size, max_iterations)
 
     # Animation
+    # animate(graph_1, color_set_size, max_iterations, pos, graph_1_name)
+
