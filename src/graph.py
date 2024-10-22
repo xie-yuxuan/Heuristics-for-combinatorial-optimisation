1d7c9efb28fc9261871158d05acbd614bbcbb125
Yuxuan Xie <yx353@cam.ac.uk>
2024-10-22 22:20:09 +0100

    get adj matrix from graph data

--- src/graph.py
+++ src/graph.py
@@ -19,14 +19,16 @@ def load_graph_from_json(file_path):
     
     graph_data = data["graph_data"]
     graph = json_graph.node_link_graph(graph_data)
+
+    # uncomment to get adj matrix
+    # adj_matrix = nx.adjacency_matrix(graph, weight='weight').toarray()
     
     return graph, graph_name, color_set_size, degree, num_nodes, gaussian_mean, gaussian_variance
-
 
 if __name__ == '__main__':
     file_path = r"C:\Projects\Heuristics for combinatorial optimisation\Heuristics-for-combinatorial-optimisation\data\graphs\test3.json"
     graph, graph_name, color_set_size, degree, num_nodes, gaussian_mean, gaussian_variance = load_graph_from_json(file_path)
     
-    # Uncomment line below to view graph plot
+    # uncomment to visualise graph plot
     # draw_graph(graph, pos=nx.spring_layout(graph, seed=1), graph_name=graph_name, iterations_taken=0, cost_data=None)
 
