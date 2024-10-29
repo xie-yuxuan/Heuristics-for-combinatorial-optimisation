c7a1ccaafc5322050a9849f8ef395b8e54084743
Yuxuan Xie <yx353@cam.ac.uk>
2024-10-29 23:40:11 +0000

    finished new approach, clean up code later

--- src/graph.py
+++ src/graph.py
@@ -27,15 +27,15 @@ def load_graph_from_json(file_path):
     return graph, graph_name, color_set_size, degree, num_nodes, gaussian_mean, gaussian_variance
 
 if __name__ == '__main__':
-    file_path = r"C:\Projects\Heuristics for combinatorial optimisation\Heuristics-for-combinatorial-optimisation\data\graphs\test4.json"
+    file_path = r"C:\Projects\Heuristics for combinatorial optimisation\Heuristics-for-combinatorial-optimisation\data\graphs\test6.json"
     graph, graph_name, color_set_size, degree, num_nodes, gaussian_mean, gaussian_variance = load_graph_from_json(file_path)
     # uncomment to visualise graph plot bef optimisation
     # draw_graph(graph, pos=nx.spring_layout(graph, seed=1), graph_name=graph_name, iterations_taken=0, cost_data=None)
 
 
     # graph, final_cost, iterations_taken, cost_data = optimise(graph, color_set_size, algo = 'reluctant')
-    graph, final_cost, iterations_taken, cost_data = optimise2(graph, color_set_size, algo = 'greedy')
+    graph, final_cost, iterations_taken, cost_data = optimise2(graph, color_set_size, algo = 'reluctant')
 
     # uncomment to visualise graph plot aft optimisation
-    # draw_graph(graph, pos=nx.spring_layout(graph, seed=1), graph_name=graph_name, iterations_taken=iterations_taken, cost_data=cost_data)
+    draw_graph(graph, pos=nx.spring_layout(graph, seed=1), graph_name=graph_name, iterations_taken=iterations_taken, cost_data=cost_data)
     # draw_graph(graph, pos=nx.spring_layout(graph, seed=1), graph_name=graph_name, iterations_taken=0, cost_data=None)
