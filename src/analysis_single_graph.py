ac55ddb123f0e880a793a9af79359fc0ca64d61b
Yuxuan Xie <yx353@cam.ac.uk>
2024-11-15 23:45:59 +0000

    processed (2000, 20, 8)

--- src/analysis_single_graph.py
+++ src/analysis_single_graph.py
@@ -110,7 +110,7 @@ def calculate_greedy_vs_reluctant_stats(cost_data):
 
 if __name__ == '__main__':
 
-    file_path = r"C:\Projects\Heuristics for combinatorial optimisation\Heuristics-for-combinatorial-optimisation\results\(100, 20, 2)_results.json"
+    file_path = r"C:\Projects\Heuristics for combinatorial optimisation\Heuristics-for-combinatorial-optimisation\results\(100, 20, 8)_results.json"
 
     with open(file_path, 'r') as f:
         data = json.load(f)
@@ -131,7 +131,7 @@ if __name__ == '__main__':
         print(f"Reluctant found global optima. Final cost: {best_costs['Reluctant']['Best Final Cost']} vs {best_costs['Greedy']['Best Final Cost']}")
     
     # visualisation: plot cost against iterations for all initial colorings 
-    plot_cost_data(all_cost_data, graph_name, color_set_size, degree, num_nodes, gaussian_mean, gaussian_variance, specific_coloring=0)
+    plot_cost_data(all_cost_data, graph_name, color_set_size, degree, num_nodes, gaussian_mean, gaussian_variance, specific_coloring=None)
 
     stats = calculate_greedy_vs_reluctant_stats(data["cost_data"])
 
@@ -141,16 +141,16 @@ if __name__ == '__main__':
     print(f"Average cost difference (Greedy - Reluctant): {stats['avg_cost_difference']}")
 
     # visualisation: plot scatter of final costs against initial colorings
-    # plot_final_costs(
-    #     cost_data=data["cost_data"],
-    #     graph_name=graph_name,
-    #     degree=degree,
-    #     num_nodes=num_nodes,
-    #     color_set_size=color_set_size,
-    #     gaussian_mean=gaussian_mean,
-    #     gaussian_variance=gaussian_variance
-    # )
+    plot_final_costs(
+        cost_data=data["cost_data"],
+        graph_name=graph_name,
+        degree=degree,
+        num_nodes=num_nodes,
+        color_set_size=color_set_size,
+        gaussian_mean=gaussian_mean,
+        gaussian_variance=gaussian_variance
+    )
 
     # visualisation: plot histogram of cost differences for all initial colorings
-    # plot_cost_diff_histogram(data["cost_data"], num_nodes, graph_name, num_bins=100, bin_range=(-0.5, 0.5))
+    plot_cost_diff_histogram(data["cost_data"], num_nodes, graph_name, num_bins=100, bin_range=(-0.5, 0.5))
 
